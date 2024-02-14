5067c6ed6424dd26ddfc71ba36c57884ed914a2e
mygit@23 <naveenmadhesan23@gmail.com>
2024-02-14 10:36:49 +0530

    Add project files.

---
+++ CalculationLibrary/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculationLibrary
+{
+    public class Calculator
+    {
+        public double Add(double num1, double num2)
+        {
+            double result = num1 + num2;
+            return result;
+        }
+        public double Sub(double num1, double num2)
+        {
+            double result = num1 - num2;
+            return result;
+        }
+        public double Multi(double num1, double num2)
+        {
+            double result = num1 * num2;
+            return result;
+        }
+        public double Divid(double num1, double num2)
+        {
+            double result = num1 / num2;
+            return result;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using CalculationLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Calculator calc;
+        public UnitTest1(){
+            calc=new Calculator();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int num1 = 20;
+            int num2 = 20;
+            Assert.AreEqual(num1, num2);
+        }
+        [TestMethod]
+        public void AddMethod() {
+            double num1 = 5;
+            double num2 = 10;
+            double result = 15;
+            Assert.AreEqual(result, calc.Add(num1,num2));
+        }
+        [TestMethod]
+        public void SubMethod()
+        {
+            double num1 = 5;
+            double num2 = 10;
+            double result = -5;
+            Assert.AreEqual(result, calc.Sub(num1, num2));
+        }
+        [TestMethod]
+        public void MultiplyMethod()
+        {
+            double num1 = 5;
+            double num2 = 10;
+            double result = 50;
+            Assert.AreEqual(result, calc.Multi(num1, num2));
+        }
+        [TestMethod]
+        public void DividMethod()
+        {
+            double num1 = 5;
+            double num2 = 10;
+            double result = 0.50;
+            Assert.AreEqual(result, calc.Divid(num1, num2));
+        }
+    }
+}
